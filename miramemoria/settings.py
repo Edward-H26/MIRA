97e9325f20be16b60c48a65e2d4e5031cdb746b4
HerobrineXia <HerobrineXia@outlook.com>
2026-01-28 12:47:17 -0600

    Refactor app's folder and initial setup

--- miramemoria/settings.py
+++ miramemoria/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'Mira.apps.MiraConfig',
+    'app.Mira.apps.MiraConfig',
 ]
 
 MIDDLEWARE = [
