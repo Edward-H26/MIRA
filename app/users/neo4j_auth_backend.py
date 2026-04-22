20f494c2bb11ba2fe6b0c02ac7537025513a080c
Edward-H26 <qiranhu8@gmail.com>
2026-04-22 12:28:31 -0500

    fix login

--- app/users/neo4j_auth_backend.py
+++ app/users/neo4j_auth_backend.py
@@ -22,7 +22,7 @@ class Neo4jAuthenticationBackend:
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
+        except (User.DoesNotExist, ValueError, TypeError):
             return None
 
     @staticmethod
